377d233d645a84f04f10f52e77bb0e5c799e7a27
LeMinhHung120204 <lmhung777@gmail.com>
2025-07-03 23:37:54 +0700

    update bru

--- core/bru/bru.py
+++ core/bru/bru.py
@@ -1,20 +1,20 @@
-from pht import PHT
-from btb import BTB
-from ghr import GHR
+from .pht import PHT
+from .btb import BTB
+from .ghr import GHR
 
-class GshareBranchPredictor:
+class BRU:
     def __init__(self, ghr_bits=8, pht_size=256, btb_size=32):
-        self.ghr = GHR(bits=ghr_bits)
-        self.pht = PHT(size=pht_size)
-        self.btb = BTB(size=btb_size)
-
+        self.ghr = GHR(bits=ghr_bits) # Số bit của GHR
+        self.pht = PHT(size=pht_size) # Số lượng mục trong PHT
+        self.btb = BTB(size=btb_size) # Số dòng trong bảng BTB
+        
     def predict(self, pc):
         ghr_value = self.ghr.read()
         index = (pc >> 2) ^ ghr_value   # Gshare = PC xor GHR
-        direction = self.pht.predict(index)  # Taken or Not Taken
+        taken = self.pht.predict(index)  # Taken or Not Taken
 
         hit, target = self.btb.lookup(pc)
-        if direction and hit:
+        if taken and hit:
             return True, target  # Taken → dùng target từ BTB
         else:
             return False, pc + 4  # Not taken → PC + 4
