cedfb48e75bca368da0f2dfe0deb8906cde88e67
LeMinhHung120204 <lmhung777@gmail.com>
2025-08-10 17:25:05 +0700

    update plru cache

--- Verilog/RegFile.v
+++ Verilog/RegFile.v
@@ -13,40 +13,11 @@ module RegFile(
         rd2 = (rd != 5'b0 && rd == rs2 && we) ? wd : register[rs2];
     end
 
+    integer i;
     always @(posedge clk or negedge rst) begin
         if (!rst) begin
-            register[0] <= 32'd0;
-            register[1] <= 32'd0;
-            register[2] <= 32'd448;
-            register[3] <= 32'd0;
-            register[4] <= 32'd0;
-            register[5] <= 32'd0;
-            register[6] <= 32'd0;
-            register[7] <= 32'd0;
-            register[8] <= 32'd0;
-            register[9] <= 32'd0;
-            register[10] <= 32'd0;
-            register[11] <= 32'd0;
-            register[12] <= 32'd0;
-            register[13] <= 32'd0;
-            register[14] <= 32'd0;
-            register[15] <= 32'd0;
-            register[16] <= 32'd0;
-            register[17] <= 32'd0;
-            register[18] <= 32'd0;
-            register[19] <= 32'd0;
-            register[20] <= 32'd0;
-            register[21] <= 32'd0;
-            register[22] <= 32'd0;
-            register[23] <= 32'd0;
-            register[24] <= 32'd0;
-            register[25] <= 32'd0;
-            register[26] <= 32'd0;
-            register[27] <= 32'd0;
-            register[28] <= 32'd0;
-            register[29] <= 32'd0;
-            register[30] <= 32'd0;
-            register[31] <= 32'd0; 
+            for (i = 0; i < 32; i = i + 1'b1)
+                register[i] <= 32'b0;
         end else if (we && rd != 5'b00000) begin
              register[rd] <= wd; 
         end
