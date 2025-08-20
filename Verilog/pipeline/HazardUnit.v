423480b028401bb5dbba5a6ca8bbeb55d15f2976
LeMinhHung120204 <lmhung777@gmail.com>
2025-08-20 19:10:08 +0700

    update RV32I pipeline with hazard unit

--- Verilog/pipeline/HazardUnit.v
+++ Verilog/pipeline/HazardUnit.v
@@ -2,11 +2,13 @@
 module HazardUnit #(
     parameter DATA_WIDTH = 32
 )(
-    input M_RegWrite, W_RegWrite,
-    input [4:0] E_Rs1, E_Rs2, M_Rd, W_Rd,
-    output reg [1:0] ForwardAE, ForwardBE       // forward cho SrcAE, SrcBE
-);
+    input M_RegWrite, W_RegWrite, E_ResultSrc_0, E_PCSrc, 
+    input [4:0] D_Rs1, D_Rs2, E_Rs1, E_Rs2, E_Rd, M_Rd, W_Rd,
+    output reg [1:0] ForwardAE, ForwardBE,       // forward cho SrcAE, SrcBE
+    output F_Stall, D_Stall, D_Flush, E_Flush
 
+);
+    // Solve Data Hazard
     always @(*) begin
         if ((E_Rs1 == M_Rd) & M_RegWrite & (E_Rs1 != 5'd0)) begin       //  Forward tu Memory stage
             ForwardAE = 2'b10;
@@ -28,4 +30,14 @@ module HazardUnit #(
             ForwardBE = 2'b00;
         end  
     end 
+
+    // Stall when a load hazard
+    wire lw_Stall;
+    assign lw_Stall = E_ResultSrc_0 & ((D_Rs1 == E_Rd) | (D_Rs2 == E_Rd));
+    assign F_Stall  = lw_Stall | E_PCSrc;
+    assign D_Stall  = lw_Stall;
+
+    // flush khi nhanh duoc lay hoac khi lenh lw duoc thuc thi tao load hazard
+    assign E_Flush  = lw_Stall;
+    assign D_Flush  = E_PCSrc;
 endmodule 
