c45b5587a30bf6bd6d99e9d78b0bb689f9dc19e2
LeMinhHung120204 <lmhung777@gmail.com>
2025-08-23 15:35:40 +0700

    done rv32i

--- Verilog/pipeline/tb_RV32I.v
+++ Verilog/pipeline/tb_RV32I.v
@@ -2,7 +2,7 @@
 
 module tb_RV32I;
     reg clk, rst_n;
-    reg[31:0] r0, r1, r2, r3, r4, r5, r6, r7, r8;
+    reg[10:0] count_clock;
 
     RV32I dut(
         .clk(clk),
@@ -10,17 +10,13 @@ module tb_RV32I;
     );
 
     always #5 clk = ~clk;
-
-    always @(posedge clk) begin
-        r0 = dut.register_file.register[0];
-        r1 = dut.register_file.register[1];
-        r2 = dut.register_file.register[2];
-        r3 = dut.register_file.register[3];
-        r4 = dut.register_file.register[4];
-        r5 = dut.register_file.register[5];
-        r6 = dut.register_file.register[6];
-        r7 = dut.register_file.register[7];
-        r8 = dut.register_file.register[8];
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            count_clock <= 11'd0;
+        end 
+        else begin
+            count_clock <= count_clock + 1'b1;
+        end 
     end
 
     initial begin
@@ -29,6 +25,6 @@ module tb_RV32I;
 
     #50 rst_n = 1;
 
-    #10000;
+    #5000;
     end
 endmodule 
