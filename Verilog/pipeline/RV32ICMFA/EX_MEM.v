48e367c941af0f6bd779e82e945b9d2e282bd03a
LeMinhHung120204 <lmhung777@gmail.com>
2025-09-23 23:19:00 +0700

    git add RV32IM 142.86 Mhz , not solving hazard for mul/div/remainder

--- Verilog/pipeline/RV32ICMFA/EX_MEM.v
+++ Verilog/pipeline/RV32ICMFA/EX_MEM.v
@@ -4,18 +4,18 @@ module EX_MEM #(
     parameter ADDR_WIDTH = 32
 )(
     input   clk, rst_n, E_RegWrite, E_MemWrite,
-    input   [DATA_WIDTH - 1:0]  E_ALUResult, E_WriteData, E_ImmExt,
+    input   [DATA_WIDTH - 1:0]  E_ALUResult, E_WriteData, E_ImmExt, E_MulOut, E_quotient, E_remainder,
     input   [ADDR_WIDTH - 1:0]  E_PCPlus4, E_PCTarget,
     input   [4:0]               E_Rd,
     input   [2:0]               E_ResultSrc, E_StoreSrc,
 
-    output  [DATA_WIDTH - 1:0]  M_ALUResult, M_WriteData, M_ImmExt,
+    output  [DATA_WIDTH - 1:0]  M_ALUResult, M_WriteData, M_ImmExt, M_MulOut, M_quotient, M_remainder,
     output  [ADDR_WIDTH - 1:0]  M_PCPlus4, M_PCTarget,
     output  [4:0]               M_Rd,
     output  [2:0]               M_ResultSrc, M_StoreSrc,
     output M_RegWrite, M_MemWrite
 );
-    reg [DATA_WIDTH - 1:0] reg_ALUResultM, reg_WriteDataM, reg_ImmExtM;
+    reg [DATA_WIDTH - 1:0] reg_ALUResultM, reg_WriteDataM, reg_ImmExtM, reg_MulOutM, reg_quotientM, reg_remainderM;
     reg [ADDR_WIDTH - 1:0] reg_PCPlus4M, reg_PCTargetM;
     reg [4:0] reg_M_rd;
     reg [2:0] reg_ResultSrcM, reg_StoreSrcM;
@@ -29,6 +29,9 @@ module EX_MEM #(
             reg_ImmExtM     <= 32'd0;
             reg_PCPlus4M    <= 32'd0;
             reg_PCTargetM   <= 32'd0;
+            reg_MulOutM     <= 32'd0;
+            reg_quotientM   <= 32'd0;
+            reg_remainderM  <= 32'd0;
             reg_M_rd        <= 5'd0;
             reg_RegWriteM   <= 1'b0;
             reg_MemWriteM   <= 1'b0;
@@ -41,6 +44,9 @@ module EX_MEM #(
             reg_ImmExtM     <= E_ImmExt;
             reg_PCPlus4M    <= E_PCPlus4;
             reg_PCTargetM   <= E_PCTarget;
+            reg_MulOutM     <= E_MulOut;
+            reg_quotientM   <= E_quotient;
+            reg_remainderM  <= E_remainder;
             reg_M_rd        <= E_Rd;
             reg_RegWriteM   <= E_RegWrite;
             reg_MemWriteM   <= E_MemWrite;
@@ -54,6 +60,9 @@ module EX_MEM #(
     assign M_ImmExt     = reg_ImmExtM;
     assign M_PCPlus4    = reg_PCPlus4M;
     assign M_PCTarget   = reg_PCTargetM;
+    assign M_MulOut     = reg_MulOutM;
+    assign M_quotient   = reg_quotientM;
+    assign M_remainder  = reg_remainderM;
     assign M_Rd         = reg_M_rd;
     assign M_RegWrite   = reg_RegWriteM;
     assign M_MemWrite   = reg_MemWriteM;
