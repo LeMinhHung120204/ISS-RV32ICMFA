dd16e353abaa4453ec1a6156492bc292c5c75674
zalaw1610 <22520830@gm.uit.edu.vn>
2025-10-14 15:10:39 +0700

    fix

---
+++ RV32ICMFA/RV32ICMFA.srcs/sources_1/imports/pipeline/Float/fadd.v
@@ -0,0 +1,289 @@
+`timescale 1ns/1ps
+module fadd #(
+    parameter WIDTH = 32
+)(
+    input   clk, rst_n, valid_input,
+    input   [WIDTH-1:0] a, b,
+    output  valid_output,
+    output  [WIDTH-1:0] y
+);
+    reg [3:0]       state;
+    localparam  get_input       = 4'd0,
+                unpack          = 4'd1,
+                special_cases   = 4'd2,
+                align           = 4'd3,
+                add_0           = 4'd4,
+                add_1           = 4'd5,
+                normalize_1     = 4'd6,
+                normalize_2     = 4'd7,
+                round           = 4'd8,
+                pack            = 4'd9,
+                put_z           = 4'd10;
+    
+    reg [WIDTH-1:0] a_reg, b_reg, res, reg_oY;
+    reg [27:0]      sum;
+    reg [26:0]      a_m, b_m;           // {mantissa, guard, round, sticky}
+    reg [23:0]      res_m;
+    reg [8:0]       a_e, b_e, res_e;
+    reg             a_s, b_s, res_s;
+    reg             guard, round_bit, sticky, reg_oValid;
+
+    assign y            = reg_oY;
+    assign valid_output = reg_oValid;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (~rst_n) begin
+            state       <= get_input;
+            a_reg       <= 32'd0;
+            b_reg       <= 32'd0;
+            res         <= 32'd0;
+            reg_oY      <= 32'd0;
+            sum         <= 28'd0;
+            a_m         <= 27'd0;
+            b_m         <= 27'd0;
+            res_m       <= 24'd0;
+            a_e         <= 9'd0;
+            b_e         <= 9'd0;
+            res_e       <= 9'd0;
+            a_s         <= 1'b0;
+            b_s         <= 1'b0;
+            res_s       <= 1'b0;
+            guard       <= 1'b0;
+            round_bit   <= 1'b0;
+            sticky      <= 1'b0;
+            reg_oValid  <= 1'b0;
+
+        end 
+        else begin
+            case(state)
+                get_input: begin
+                    a_reg       <= 32'd0;
+                    b_reg       <= 32'd0;
+                    res         <= 32'd0;
+                    reg_oY      <= 32'd0;
+                    sum         <= 28'd0;
+                    a_m         <= 27'd0;
+                    b_m         <= 27'd0;
+                    res_m       <= 24'd0;
+                    a_e         <= 9'd0;
+                    b_e         <= 9'd0;
+                    res_e       <= 9'd0;
+                    a_s         <= 1'b0;
+                    b_s         <= 1'b0;
+                    res_s       <= 1'b0;
+                    guard       <= 1'b0;
+                    round_bit   <= 1'b0;
+                    sticky      <= 1'b0;
+                    reg_oValid  <= 1'b0;
+                    if (valid_input) begin
+                        a_reg <= a;
+                        b_reg <= b;
+                        state <= unpack;
+                    end 
+                end 
+                unpack: begin
+                    a_m   <= {1'b0, a_reg[22:0], 3'b000};
+                    b_m   <= {1'b0, b_reg[22:0], 3'b000};
+                    a_e   <= a_reg[30:23] - 8'd127;
+                    b_e   <= b_reg[30:23] - 8'd127;
+                    a_s   <= a_reg[31];
+                    b_s   <= b_reg[31];
+                    state <= special_cases;
+                end 
+                special_cases: begin
+                    // if a is NaN or b is NaN return NaN (Not a Number)
+                    if ((a_e == 128 && a_m !=0) || (b_e == 128 && b_m != 0)) begin
+                        // res[31]     <= 1'b1;
+                        // res[30:23]  <= 8'd255;
+                        // res[22]     <= 1'b1;
+                        // res[21:0]   <= 22'd0;
+                        res     <= 32'h7FC00000;
+                        state   <= put_z;
+                    end 
+
+                    // if a is inf return inf
+                    else if (a_e == 128) begin
+                        res[31]    <= a_s;
+                        res[30:23] <= 255;
+                        res[22:0]  <= 0;
+
+                        //if a is inf and signs don't match return nan
+                        if ((b_e == 128) && (a_s != b_s)) begin
+                            // res[31]     <= b_s;
+                            // res[30:23]  <= 8'd255;
+                            // res[22]     <= 1'b1;
+                            // res[21:0]   <= 22'd0;
+                            res     <= 32'h7FC00000;
+                        end
+                        state <= put_z;
+                    end 
+
+                    //if b is inf return inf
+                    else if(b_e == 128) begin
+                        res[31]     <= b_s;
+                        res[30:23]  <= 8'd255;
+                        res[22:0]   <= 23'd0;
+                        state       <= put_z;
+                    end 
+
+                    //if a, b is zero return b
+                    else if(($signed(a_e) == -127) && (a_m == 26'd0) && ($signed(b_e) == -127) && (b_m == 26'd0)) begin
+                        res[31]     <= a_s & b_s;
+                        res[30:23]  <= b_e[7:0] + 8'd127;
+                        res[22:0]   <= b_m[26:3];
+                        state       <= put_z;
+                    end 
+
+                    //if a is zero return b
+                    else if(($signed(a_e) == -127) && (a_m == 26'd0)) begin
+                        res[31]     <= b_s;
+                        res[30:23]  <= b_e[7:0] + 8'd127;
+                        res[22:0]   <= b_m[26:3];
+                        state       <= put_z;
+                    end 
+
+                    //if b is zero return a
+                    else if(($signed(b_e) == -127) && (b_m == 26'd0)) begin
+                        res[31]     <= a_s;
+                        res[30:23]  <= a_e[7:0] + 8'd127;
+                        res[22:0]   <= a_m[26:3];
+                        state       <= put_z;
+                    end 
+
+                    else begin
+                        if ($signed(a_e) == -127) begin
+                            a_e     <= -126; // exponent cho subnormal
+                        end 
+                        else begin
+                            a_m[26] <= 1'b1; // them hidden bit neu la normalized
+                        end 
+
+                        if ($signed(b_e) == -127) begin
+                            b_e     <= -126;
+                        end 
+                        else begin
+                            b_m[26] <= 1'b1;
+                        end 
+                        state       <= align;
+                    end 
+                end 
+                
+                align: begin
+                    if ($signed(a_e) > $signed(b_e)) begin
+                        b_e     <= b_e + 1'b1;
+                        b_m     <= b_m >> 1;
+                        b_m[0]  <= b_m[0] | b_m[1];
+                    end 
+                    else if ($signed(a_e) < $signed(b_e)) begin
+                        a_e     <= a_e + 1'b1;
+                        a_m     <= a_m >> 1;
+                        a_m[0]  <= a_m[0] | a_m[1];
+                    end 
+                    else begin
+                        state <= add_0;
+                    end 
+                end 
+
+                add_0: begin
+                    res_e       <= a_e;
+                    if (a_s == b_s) begin
+                        sum     <= a_m + b_m;
+                        res_s   <= a_s;
+                    end 
+                    else begin
+                        if (a_m >= b_m) begin
+                            sum     <= a_m - b_m;
+                            res_s   <= a_s;
+                        end 
+                        else begin
+                            sum     <= b_m - a_m;
+                            res_s   <= b_s;
+                        end 
+                    end 
+                    state <= add_1;
+                end 
+
+                add_1: begin
+                    if (sum[27]) begin
+                        res_m       <= sum[27:4];
+                        guard       <= sum[3];
+                        round_bit   <= sum[2];
+                        sticky      <= sum[1] | sum[0];
+                    end 
+                    else begin
+                        res_m       <= sum[26:3];
+                        guard       <= sum[2];
+                        round_bit   <= sum[1];
+                        sticky      <= sum[0];
+                    end 
+                    state <= normalize_1;
+                end 
+
+                normalize_1: begin
+                    if (res_m[23] == 0 && $signed(res_e) > -126) begin
+                        res_e       <= res_e - 1'b1;
+                        res_m       <= res_m << 1;
+                        res_m[0]    <= guard;
+                        guard       <= round_bit;
+                        round_bit   <= 1'b0;
+                    end 
+                    else begin
+                        state <= normalize_2;
+                    end 
+                end
+
+                normalize_2: begin
+                    if ($signed(res_e) < -126) begin
+                        res_e       <= res_e + 1'b1;
+                        res_m       <= res_m >> 1;
+                        guard       <= res_m[0];
+                        round_bit   <= guard;
+                        sticky      <= sticky | round_bit;
+                    end 
+                    else begin
+                        state <= round;
+                    end 
+                end 
+
+                round: begin
+                    if (guard && (round_bit | sticky | res_m[0])) begin
+                        res_m <= res_m + 1'b1;
+                        if (res_m == 24'hffffff) begin
+                            res_e <= res_e + 1'b1;
+                        end 
+                    end 
+                    else begin
+                        state <= pack;
+                    end 
+                end 
+
+                pack: begin
+                    res[22:0]   <= res_m[22:0];
+                    res[30:23]  <= res_e[7:0] + 8'd127;
+                    res[31]     <= res_s;
+                    // return 0
+                    if ($signed(res_e) == -126 && res_m[23] == 1'b0) begin
+                        res[30:23]  <= 8'd0;
+                    end 
+                    if ($signed(res_e) == -126 && res_m[23:0] == 24'd0) begin
+                        res[31] <= 1'b0;    // -a + a = +0
+                    end 
+
+                    if ($signed(res_e) > 127) begin
+                        res[22:0]   <= 23'd0;
+                        res[30:23]  <= 8'd255;
+                        res[31]     <= res_s;
+                    end 
+                    state   <= put_z;
+                end 
+
+                put_z: begin
+                    reg_oY      <= res;
+                    reg_oValid  <= 1'b1;
+                    state       <= get_input;
+                end
+            endcase
+        end 
+    end 
+
+endmodule 
---
+++ RV32ICMFA/RV32ICMFA.srcs/sources_1/imports/pipeline/RV32ICMFA/RV32I.v
@@ -0,0 +1,372 @@
+`timescale 1ns/1ps
+module RV32I #(
+    parameter WIDTH_DATA = 32,
+    parameter WIDTH_ADDR = 32
+)(
+    input   clk, rst_n,
+    output  W_Result_output
+    // input   [WIDTH_DATA - 1:0]  imem_instr, dmem_rdata,
+    // output  [WIDTH_ADDR - 1:0]  imem_addr, 
+    // output  [WIDTH_DATA - 1:0]  dmem_wdata,
+    // output  [7:0]               dmem_addr,
+    // output                      dmem_we
+);
+    wire [WIDTH_DATA - 1:0] RD1, RD2, F_RD, E_RD1, E_RD2;
+    wire [WIDTH_DATA - 1:0] D_Instr, M_ReadData, W_ReadData;
+    wire [WIDTH_DATA - 1:0] D_ImmExt, E_ImmExt, M_ImmExt, W_ImmExt, E_ALUResult, M_ALUResult, W_ALUResult, M_WriteData;
+    wire [4:0]              E_Rs1, E_Rs2, E_Rd, M_Rd, W_Rd, A1, A2, WD3;
+
+    wire [WIDTH_ADDR - 1:0] F_PC, D_PC, E_PC ,PCNext, F_PCPlus4, D_PCPlus4, E_PCPlus4, M_PCPlus4, W_PCPlus4, E_PCTarget, M_PCTarget, W_PCTarget;
+    wire [WIDTH_DATA - 1:0] W_Result, E_SrcA, E_SrcB;
+    wire [WIDTH_DATA - 1:0] E_WriteData;
+
+    // ----------------------- Tin hieu dieu khien -----------------------
+    wire D_RegWrite, D_MemWrite, D_Jump, D_Branch, D_ALUSrc;
+    wire E_signed_less, E_RegWrite, E_MemWrite, E_Jump, E_Branch, E_ALUSrc, E_Zero, E_PCSrc;
+    wire M_RegWrite, M_MemWrite;
+    wire W_RegWrite;
+    wire PCTargetSrc;
+    wire [2:0] D_ResultSrc, E_ResultSrc, M_ResultSrc, W_ResultSrc;
+    wire [2:0] D_ImmSrc, E_funct3;
+    wire [3:0] D_ALUControl, E_ALUControl;
+    wire [2:0] D_StoreSrc, E_StoreSrc, M_StoreSrc;
+
+    // ----------------------- Tin hieu dieu khien MulDiv -----------------------
+    wire [WIDTH_DATA - 1:0] E_MulHigh, E_MulLow, E_MulOut, M_MulOut, W_MulOut;
+    wire [WIDTH_DATA - 1:0] E_quotient, M_quotient, W_quotient;
+    wire [WIDTH_DATA - 1:0] E_remainder, M_remainder, W_remainder;
+    wire [WIDTH_DATA - 1:0] OutData;
+    wire [1:0] Mul_Div_unsigned;
+    wire is_high;
+
+    // ----------------------- Tin hieu Hazard -----------------------
+    wire F_Stall, D_Stall, D_Flush, E_Flush;
+    wire [1:0] ForwardAE, ForwardBE;
+
+    // ----------------------- Tin hieu PC -----------------------
+    assign F_PCPlus4    = F_PC + 32'd4;
+    mux4_1 mux4_1_PC(
+        .in0(F_PCPlus4),
+        .in1(F_PCPlus4),
+        .in2(E_PC + E_ImmExt),
+        .in3(E_ALUResult),
+        .sel({E_PCSrc, PCTargetSrc}),
+        .res(PCNext)
+    );
+
+    assign W_Result_output = W_Result;
+    // assign E_PCTarget   = (PCTargetSrc == 1'b1) ? E_ALUResult   : E_PC + E_ImmExt;
+    // assign PCNext       = (E_PCSrc == 1'b1)     ? E_PCTarget    : F_PCPlus4;
+    
+    // ----------------------- Tin Hieu Ins_Mem -----------------------
+    // assign imem_addr    = F_PC;
+    // assign F_RD         = imem_instr;
+
+    // ----------------------- Tin Hieu Data Mem -----------------------
+    // assign dmem_we      = M_MemWrite;
+    // assign dmem_addr    = M_ALUResult[7:0];
+    // assign dmem_wdata   = M_WriteData;
+    // assign M_ReadData   = dmem_rdata;
+
+    // ----------------------------------------------
+    assign A1   = D_Instr[19:15];
+    assign A2   = D_Instr[24:20];
+    assign WD3  = D_Instr[11:7];
+
+    // mux4_1 mux_W_Result (
+    //     .in0(W_ALUResult),
+    //     .in1(W_ReadData),
+    //     .in2(W_PCPlus4),
+    //     .in3(32'd0),
+    //     .sel(W_ResultSrc),
+    //     .res(W_Result)
+    // );
+
+    mux8_1 mux_W_Result (
+        .in0(W_ALUResult),
+        .in1(W_ReadData),
+        .in2(W_PCPlus4),
+        .in3(W_ImmExt),
+        .in4(W_PCTarget),
+        .in5(W_MulOut),
+        .in6(W_quotient),
+        .in7(W_remainder),
+        .sel(W_ResultSrc),
+        .res(W_Result)
+    );
+
+    mux4_1 mux_ForwardAE (
+        .in0(E_RD1),
+        .in1(W_Result),
+        .in2(M_ALUResult),
+        .in3(32'd0),
+        .sel(ForwardAE),
+        .res(E_SrcA)
+    );
+
+    mux4_1 mux_ForwardBE (
+        .in0(E_RD2),
+        .in1(W_Result),
+        .in2(M_ALUResult),
+        .in3(32'd0),
+        .sel(ForwardBE),
+        .res(E_WriteData)
+    );
+
+    mux2_1 mux_E_ALUSrc (
+        .in0(E_WriteData),
+        .in1(E_ImmExt),
+        .sel(E_ALUSrc),
+        .res(E_SrcB)
+    );
+
+    mux2_1 mux_E_MulOut (
+        .in0(E_MulLow),
+        .in1(E_MulHigh),
+        .sel(is_high),
+        .res(E_MulOut)
+    );
+
+    HazardUnit HazardUnit_inst(
+        .D_Rs1(A1),
+        .D_Rs2(A2),
+        .E_Rs1(E_Rs1),
+        .E_Rs2(E_Rs2),
+        .E_Rd(E_Rd),
+        .E_PCSrc(E_PCSrc),
+        .E_ResultSrc(E_ResultSrc),
+        .M_RegWrite(M_RegWrite),
+        .M_Rd(M_Rd),
+        .W_Rd(W_Rd),
+        .W_RegWrite(W_RegWrite),
+        
+        .F_Stall(F_Stall),
+        .D_Stall(D_Stall),
+        .D_Flush(D_Flush),
+        .E_Flush(E_Flush),
+        .ForwardAE(ForwardAE),
+        .ForwardBE(ForwardBE)
+    );
+    
+    BranchDecoder BranchDecoder_inst(
+        .E_Jump(E_Jump),
+        .E_Zero(E_Zero),
+        .E_Branch(E_Branch),
+        .E_signed_less(E_signed_less),
+        .funct3(E_funct3),
+        .E_PCSrc(E_PCSrc)
+    );
+
+    ControlUnit ControlUnit_ins(
+        .op(D_Instr[6:0]),
+        .funct3(D_Instr[14:12]),
+        .funct7(D_Instr[31:25]),
+        .ResultSrc(D_ResultSrc),
+        .MemWrite(D_MemWrite),
+        .ALUControl(D_ALUControl),
+        .ALUSrc(D_ALUSrc),
+        .ImmSrc(D_ImmSrc),
+        .RegWrite(D_RegWrite),
+        .Branch(D_Branch),
+        .Jump(D_Jump),
+        .PCTargetSrc(PCTargetSrc),
+        .StoreSrc(D_StoreSrc),
+        .Mul_Div_unsigned(Mul_Div_unsigned),
+        .is_high(is_high)
+    );
+
+    PC PC_inst(
+        .clk(clk),
+        .rst_n(rst_n),
+        .EN(F_Stall),
+        .PCNext(PCNext),
+        .PC(F_PC)
+    );
+
+    Ins_Mem instruction_memory(
+        .addr(F_PC),
+        .instruction(F_RD)
+    );
+
+    IF_ID IF_ID_register(
+        .clk(clk),
+        .rst_n(rst_n),
+        .EN(D_Stall),
+        .D_Flush(D_Flush),
+        .F_RD(F_RD),
+        .F_PC(F_PC),
+        .F_PCPlus4(F_PCPlus4),
+        .D_Instr(D_Instr),
+        .D_PC(D_PC),
+        .D_PCPlus4(D_PCPlus4)
+    );
+
+    RegFile register_file(
+        .clk(clk),
+        .rst_n(rst_n),
+        .we(W_RegWrite),
+        .rs1(A1),
+        .rs2(A2),
+        .rd(W_Rd),
+        .wd(W_Result),
+        .rd1(RD1),
+        .rd2(RD2)
+    );
+
+    Extend extend_inst(
+        .ImmSrc(D_ImmSrc),
+        .Instr(D_Instr[31:0]),
+        .ImmExt(D_ImmExt)
+    );
+
+    ID_EX ID_EX_register(
+        .clk(clk),
+        .rst_n(rst_n),
+        .E_Flush(E_Flush),
+        .RD1(RD1),
+        .RD2(RD2),
+        .D_Rs1(A1),
+        .D_Rs2(A2),
+        .D_Rd(WD3),
+        .D_ImmExt(D_ImmExt),
+        .D_PC(D_PC),
+        .D_PCPlus4(D_PCPlus4),
+        .D_RegWrite(D_RegWrite),
+        .D_MemWrite(D_MemWrite),
+        .D_Jump(D_Jump),
+        .D_Branch(D_Branch),
+        .D_ALUSrc(D_ALUSrc),
+        .D_ResultSrc(D_ResultSrc),
+        .D_StoreSrc(D_StoreSrc),
+        // .D_ImmSrc(D_ImmSrc),
+        .D_funct3(D_Instr[14:12]),
+        .D_ALUControl(D_ALUControl),
+
+        .E_RD1(E_RD1),
+        .E_RD2(E_RD2),
+        .E_Rs1(E_Rs1),
+        .E_Rs2(E_Rs2),
+        .E_Rd(E_Rd),
+        .E_ImmExt(E_ImmExt),
+        .E_PC(E_PC),
+        .E_PCPlus4(E_PCPlus4),
+        .E_RegWrite(E_RegWrite),
+        .E_MemWrite(E_MemWrite),
+        .E_Jump(E_Jump),
+        .E_Branch(E_Branch),
+        .E_ALUSrc(E_ALUSrc),
+        .E_ResultSrc(E_ResultSrc),
+        .E_StoreSrc(E_StoreSrc),
+        // .E_ImmSrc(E_ImmSrc),
+        .E_funct3(E_funct3),
+        .E_ALUControl(E_ALUControl)
+    );
+
+    ALU alu_inst(
+        .ALUControl(E_ALUControl),
+        .in1(E_SrcA),
+        .in2(E_SrcB),
+        .result(E_ALUResult),
+        .zero(E_Zero),
+        .signed_less(E_signed_less)
+    );
+
+//     mul32 mul_inst(
+//        .clk(clk),
+//        .rst_n(rst_n),
+//        .is_unsigned(Mul_Div_unsigned),
+//        .a(E_SrcA),
+//        .b(E_WriteData),
+//        .R_high(E_MulHigh),
+//        .R_low(E_MulLow)
+//    );
+
+//     non_restore div_inst(
+//        .clk(clk),
+//        .rst_n(rst_n),
+//        .is_unsigned(Mul_Div_unsigned[0]),
+//        .dividend(E_SrcA),
+//        .divisor(E_WriteData),
+//        .quotient(E_quotient),
+//        .remainder(E_remainder)
+//     );
+    MDU MDU_inst(
+        .clk(clk),
+        .rst_n(rst_n),
+        .Mul_Div_unsigned(Mul_Div_unsigned),
+        .funct3(E_funct3),
+        .rs1(E_SrcA),
+        .rs2(E_WriteData),
+        .OutData(OutData)
+    );
+
+    EX_MEM EX_MEM_register(
+        .clk(clk),
+        .rst_n(rst_n),
+        .E_ALUResult(E_ALUResult),
+        .E_WriteData(E_WriteData),
+        .E_ImmExt(E_ImmExt),
+        .E_PCPlus4(E_PCPlus4),
+        .E_PCTarget(E_PCTarget),
+        .E_Rd(E_Rd),
+        .E_RegWrite(E_RegWrite),
+        .E_MemWrite(E_MemWrite),
+        .E_ResultSrc(E_ResultSrc),
+        .E_StoreSrc(E_StoreSrc),
+        .E_MulOut(E_MulOut),
+        .E_quotient(E_quotient),
+        .E_remainder(E_remainder),
+
+        .M_ALUResult(M_ALUResult),
+        .M_WriteData(M_WriteData),
+        .M_ImmExt(M_ImmExt),
+        .M_PCPlus4(M_PCPlus4),
+        .M_PCTarget(M_PCTarget),
+        .M_Rd(M_Rd),
+        .M_RegWrite(M_RegWrite),
+        .M_MemWrite(M_MemWrite),
+        .M_ResultSrc(M_ResultSrc),
+        .M_StoreSrc(M_StoreSrc),
+        .M_MulOut(M_MulOut),
+        .M_quotient(M_quotient),
+        .M_remainder(M_remainder)
+    );
+
+    DataMem data_memory(
+        .clk(clk),
+        .rst_n(rst_n),
+        .StoreSrc(M_StoreSrc),
+        .MemWrite(M_MemWrite),
+        .addr(M_ALUResult),
+        .data_in(M_WriteData),
+        .rd(M_ReadData)
+    );
+
+    MEM_WB MEM_WB_register(
+        .clk(clk),
+        .rst_n(rst_n),
+        .M_ALUResult(M_ALUResult),
+        .M_ReadData(M_ReadData),
+        .M_ImmExt(M_ImmExt),
+        .M_PCTarget(M_PCTarget),
+        .M_PCPlus4(M_PCPlus4),
+        .M_Rd(M_Rd),
+        .M_RegWrite(M_RegWrite),
+        .M_ResultSrc(M_ResultSrc),
+        .M_MulOut(M_MulOut),
+        .M_quotient(M_quotient),
+        .M_remainder(M_remainder),
+
+        .W_ALUResult(W_ALUResult),
+        .W_ReadData(W_ReadData),
+        .W_ImmExt(W_ImmExt),
+        .W_PCTarget(W_PCTarget),
+        .W_PCPlus4(W_PCPlus4),
+        .W_Rd(W_Rd),
+        .W_RegWrite(W_RegWrite),
+        .W_ResultSrc(W_ResultSrc),
+        .W_MulOut(W_MulOut),
+        .W_quotient(W_quotient),
+        .W_remainder(W_remainder)
+    );
+endmodule
