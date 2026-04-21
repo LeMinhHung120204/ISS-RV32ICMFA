725e48956077291d207997bac80198b9d6cc5448
zalaw1610 <22520830@gm.uit.edu.vn>
2026-04-22 02:42:11 +0700

    add hex file after spike test

--- Verilog/hexfiles/single_core/rv32i/branch/TestCase07/branch_tc07.s
+++ Verilog/hexfiles/single_core/rv32i/branch/TestCase07/branch_tc07.s
@@ -1,38 +1,32 @@
-<FILE filename="branch_tc07.s" size="5020 bytes">
     .section .text
     .globl _start
     .option norvc
-
 _start:
     ####################################################################
     # Branch group test for RV32I - TEST CASE 07
     # Extra edge cases: chained branches, large immediate compare
     ####################################################################
-
     ####################################################################
     # TEST 1-10: chained conditional branches + jalr
     ####################################################################
-    addi s0, x0, 1
-    addi t0, x0, 10
-    addi t1, x0, 0
+    li s0, 1
+    li t0, 10
+    li t1, 0
     bge  t0, x0, test1_ok
     jal  x0, fail
 test1_ok:
-
-    addi s0, x0, 2
-    addi t0, x0, -10
+    li s0, 2
+    li t0, -10
     blt  t0, x0, test2_ok
     jal  x0, fail
 test2_ok:
-
-    addi s0, x0, 3
-    addi t0, x0, 0x55555555
-    addi t1, x0, 0x55555555
+    li s0, 3
+    li t0, 0x55555555
+    li t1, 0x55555555
     beq  t0, t1, test3_ok
     jal  x0, fail
 test3_ok:
-
-    addi s0, x0, 4
+    li s0, 4
     auipc t0, 0
     addi  t0, t0, 20
     jalr  t1, 0(t0)
@@ -41,61 +35,52 @@ test4_target:
     auipc t2, 0
     addi  t2, t2, -24
     bne   t1, t2, fail
-
-    addi s0, x0, 5
-    addi t2, x0, 0
+    li s0, 5
+    li t2, 0
     jal  x0, test5_skip
-    addi t2, x0, 1
-    addi t2, x0, 2
-    addi t2, x0, 3
+    li t2, 1
+    li t2, 2
+    li t2, 3
 test5_skip:
     bne  t2, x0, fail
-
-    addi s0, x0, 6
-    addi t0, x0, 0
-    addi t1, x0, -1
+    li s0, 6
+    li t0, 0
+    li t1, -1
     bltu t0, t1, test6_ok
     jal  x0, fail
 test6_ok:
-
-    addi s0, x0, 7
-    addi t0, x0, 5
-    addi t1, x0, 0
+    li s0, 7
+    li t0, 5
+    li t1, 0
 loop7:
     addi t1, t1, 1
     addi t0, t0, -1
     bne  t0, x0, loop7
-    addi t2, x0, 5
+    li t2, 5
     bne  t1, t2, fail
-
-    addi s0, x0, 8
-    addi t0, x0, 0x80000000
-    addi t1, x0, 0x7fffffff
+    li s0, 8
+    li t0, 0x80000000
+    li t1, 0x7fffffff
     bgeu t0, t1, test8_ok
     jal  x0, fail
 test8_ok:
-
-    addi s0, x0, 9
-    addi t0, x0, -2048
-    addi t1, x0, -2048
+    li s0, 9
+    li t0, -2048
+    li t1, -2048
     beq  t0, t1, test9_ok
     jal  x0, fail
 test9_ok:
-
-    addi s0, x0, 10
-    addi t0, x0, 0
-    addi t1, x0, 1
+    li s0, 10
+    li t0, 0
+    li t1, 1
     blt  t0, t1, test10_ok
     jal  x0, fail
 test10_ok:
-
 pass:
-    addi a0, x0, 1
+    li a0, 1
 pass_loop:
     jal x0, pass_loop
-
 fail:
     add  a0, s0, x0
 fail_loop:
     jal x0, fail_loop
-</FILE>
